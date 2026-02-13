4acc613845bc735c08941950a092bad19047af82
Felix Jung <felix.jung@tu-dresden.de>
2026-02-13 08:51:46 +0100

    added a bunch of stuff

---
+++ src/revohsup/cli.py
@@ -0,0 +1,114 @@
+import logging
+from typing import Annotated
+
+import typer
+
+log = logging.getLogger(__name__)
+
+app = typer.Typer()
+
+
+@app.command("message")
+def _message(
+    message: Annotated[str, typer.Argument(help="The message to send")],
+    image_path: Annotated[
+        str,
+        typer.Option(
+            help="Path to an image to send with the message", show_default=False
+        ),
+    ] = None,
+    title: Annotated[
+        str, typer.Option(help="The title of the message", show_default=False)
+    ] = None,
+    url: Annotated[
+        str, typer.Option(help="A URL to include with the message", show_default=False)
+    ] = None,
+    url_title: Annotated[
+        str, typer.Option(help="The title of the URL", show_default=False)
+    ] = None,
+    priority: Annotated[
+        int,
+        typer.Option(
+            help="The priority of the message (-2 to 2)",
+            show_default=False,
+            min=-2,
+            max=2,
+        ),
+    ] = None,
+    timestamp: Annotated[
+        int,
+        typer.Option(
+            help="The timestamp of the message (Unix timestamp)", show_default=False
+        ),
+    ] = None,
+    retry: Annotated[
+        int,
+        typer.Option(
+            help="The retry interval for emergency priority messages (in seconds)",
+            show_default=False,
+        ),
+    ] = None,
+    expire: Annotated[
+        int,
+        typer.Option(
+            help="The expiration time for emergency priority messages (in seconds)",
+            show_default=False,
+        ),
+    ] = None,
+    ttl: Annotated[
+        int,
+        typer.Option(
+            help="Time for the message to live before being deleted automatically (in seconds)",
+            show_default=False,
+        ),
+    ] = None,
+    sound: Annotated[
+        str,
+        typer.Option(
+            help="The sound to play when the message is received", show_default=False
+        ),
+    ] = None,
+    html: Annotated[
+        bool,
+        typer.Option(help="Whether to parse the message as HTML", show_default=False),
+    ] = False,
+    markdown: Annotated[
+        bool,
+        typer.Option(
+            help="Whether to parse the message as Markdown", show_default=False
+        ),
+    ] = False,
+    token: Annotated[
+        str,
+        typer.Option(
+            help="The Pushover API token to use for sending the message",
+            show_default=False,
+        ),
+    ] = None,
+    user: Annotated[
+        str,
+        typer.Option(
+            help="The Pushover user key to send the message to", show_default=False
+        ),
+    ] = None,
+    device: Annotated[
+        str,
+        typer.Option(
+            help="The Pushover device name to send the message to", show_default=False
+        ),
+    ] = None,
+):
+    """
+    Send a message using the Pushover message api.
+
+    Parameters
+    ----------
+
+    """
+    print(message)
+
+
+@app.callback(invoke_without_command=True)
+def main(ctx: typer.Context):
+    if ctx.invoked_subcommand is None:
+        print("Welcome to revohsup. Use --help for more information.")
---
+++ src/revohsup/message.py
@@ -0,0 +1,35 @@
+import requests
+
+
+def message(
+    message: str,
+    image_path: str = None,
+    title: str = None,
+    url: str = None,
+    url_title: str = None,
+    priority: int = None,
+    timestamp: int = None,
+    retry: int = None,
+    expire: int = None,
+    ttl: int = None,
+    sound: str = None,
+    html: bool = False,
+    markdown: bool = False,
+    token: str = None,
+    user: str = None,
+    device: str = None,
+):
+    """
+    Send a message using the Pushover message api.
+
+    Parameters
+    ----------
+
+    """
+
+    r = requests.post(
+        "https://api.pushover.net/1/messages.json",
+        data={"token": "APP_TOKEN", "user": "USER_KEY", "message": "hello world"},
+        files={"attachment": ("image.jpg", open("your_image.jpg", "rb"), "image/jpeg")},
+    )
+    print(r.text)
